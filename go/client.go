4d21e2e6a86b23b8db5ec324999a64a7c49a1701
hwangseonu <hwangseonu12@naver.com>
2020-12-16 18:29:40 +0900

    add go client

---
+++ go/client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const PORT = ":7007"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("usage: %s <server ip>\n", os.Args[0])
+		os.Exit(-1)
+	}
+
+	name := ""
+	fmt.Print("Username: ")
+	fmt.Scanf("%s", &name)
+
+	conn, err := net.Dial("tcp", os.Args[1] + PORT)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	go handleSocket(conn)
+
+	for {
+		r := bufio.NewReader(os.Stdin)
+		fmt.Print("> ")
+
+		if l, _, err := r.ReadLine(); err != nil {
+			log.Fatalln(err)
+		} else if _, err := conn.Write(l); err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
+func handleSocket(conn net.Conn) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			log.Fatalln(err)
+		}
+		fmt.Println("\r" + string(line))
+		fmt.Print("> ")
+	}
+}
